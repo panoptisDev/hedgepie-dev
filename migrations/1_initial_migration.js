dd5848f26e86fddf7c26bc0417889fe1b9aae508
Carson Case <carsonpcase@gmail.com>
2021-08-05 17:37:25 +0200

    YBNFT with simple truffle tests

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
-const Migrations = artifacts.require("Migrations");
+const YBNFT = artifacts.require("YBNFT");
+const RNG = artifacts.require("RNG");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer, networks, [dev, user1, user2, lottery, treasury]) {
+  await deployer.deploy(RNG);
+  const rng = await RNG.deployed();
+  await deployer.deploy(YBNFT,treasury, lottery, rng.address, {from: dev});
 };
