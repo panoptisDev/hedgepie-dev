816d2852b1dad6ca25fa092601daab9a3fa2eb65
daiki saito <daiki.saito92@gmail.com>
2022-02-11 00:58:36 +0900

    refactor lottery contract

--- contracts/HedgepieLottery.sol
+++ contracts/HedgepieLottery.sol
@@ -6,46 +6,52 @@ import "./libraries/SafeMath.sol";
 import "./libraries/SafeBEP20.sol";
 
 interface IYBNFT {
-    function chkToken(uint tokenId) external view returns(bool);
+    function chkToken(uint256 tokenId) external view returns (bool);
 }
 
 contract HedgepieLottery is Ownable {
-    using SafeMath for uint;
+    using SafeMath for uint256;
     using SafeBEP20 for IBEP20;
 
     address public immutable HedgepieYBNFT;
     address public immutable HedgepieToken;
 
-    uint public LotteryCnt;
-    uint public ticketRatio;
+    uint256 public LotteryCnt;
+    uint256 public ticketRatio;
 
-    uint public minStake;
-    uint public maxStake;
+    uint256 public minStake;
+    uint256 public maxStake;
 
-    mapping(uint => Lottery) public Lotteries;
-    mapping(uint => mapping(address => Stake)) public LotteryUser;
+    mapping(uint256 => Lottery) public Lotteries;
+    mapping(uint256 => mapping(address => Stake)) public LotteryUser;
+
     struct Lottery {
-        string bytes lotteryName;
-        uint tokenId;
-        uint startTime;
-        uint period;
+        bytes lotteryName;
+        uint256 tokenId;
+        uint256 startTime;
+        uint256 period;
     }
 
     struct Stake {
-        uint amount;
-        uint ticket;
-        uint startTime;
+        uint256 amount;
+        uint256 ticket;
+        uint256 startTime;
     }
 
-    enum MinMax { MIN, MAX }
+    enum MinMax {
+        MIN,
+        MAX
+    }
 
-    event LotteryCreated(string bytes name, uint tokenId, uint startTime, uint period);
-    event Deposit(address indexed depositer, uint lotteryId, uint amount);
+    event LotteryCreated(
+        bytes name,
+        uint256 tokenId,
+        uint256 startTime,
+        uint256 period
+    );
+    event Deposit(address indexed depositer, uint256 lotteryId, uint256 amount);
 
-    constructor(
-        address _ybNFt,
-        address _hedgepie
-    ) {
+    constructor(address _ybNFt, address _hedgepie) {
         require(_ybNFt != address(0));
         require(_hedgepie != address(0));
 
@@ -53,19 +59,19 @@ contract HedgepieLottery is Ownable {
         HedgepieToken = _hedgepie;
     }
 
-    function setMinMax(MinMax type, uint value) external onlyOwner {
-        if(type == MinMax.MIN) {
-            minStake = value;
-        } else if(type == MinMax.MAX) {
-            maxStake = value;
+    function setMinMax(MinMax _type, uint256 _value) external onlyOwner {
+        if (_type == MinMax.MIN) {
+            minStake = _value;
+        } else if (_type == MinMax.MAX) {
+            maxStake = _value;
         }
     }
 
     function _createLottery(
-        string bytes _lotteryName, 
-        uint _tokenId, 
-        uint _startTime,
-        uint _period
+        bytes calldata _lotteryName,
+        uint256 _tokenId,
+        uint256 _startTime,
+        uint256 _period
     ) private {
         Lotteries[LotteryCnt] = Lottery({
             lotteryName: _lotteryName,
@@ -76,51 +82,56 @@ contract HedgepieLottery is Ownable {
         LotteryCnt++;
     }
 
-    function _deposit(
-        uint _lotteryId,
-        uint _amount
-    ) private {
-        address depositor = _msgSender();
-        IBEP20( HedgepieYBNFT ).safeTransferFrom(depositor, address(this), amount);
+    function _deposit(uint256 _lotteryId, uint256 _amount) private {
+        IBEP20(HedgepieYBNFT).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
 
-        Stake memory info = LotteryUser[lotteryId][depositor];
-        LotteryUser[lotteryId][depositor] = Stake({
+        Stake memory info = LotteryUser[_lotteryId][msg.sender];
+        LotteryUser[_lotteryId][msg.sender] = Stake({
             amount: info.amount.add(_amount),
-            ticket: info.ticket.add(getUserTicket(lotteryId, depositor)),
+            ticket: info.ticket.add(getUserTicket(_lotteryId, msg.sender)),
             startTime: block.timestamp
         });
     }
 
-    function getUserTicket(uint lotteryId, address usesr) external view returns(uint) {
-        Stake memory info = LotteryUser[lotteryId][user];
-        uint diffAmount = info.amount.mul(block.timestamp.sub(startTime));
+    function getUserTicket(uint256 _lotteryId, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        Stake memory info = LotteryUser[_lotteryId][_user];
+        uint256 diffAmount = info.amount.mul(
+            block.timestamp.sub(info.startTime)
+        );
         return info.ticket.add(diffAmount);
     }
 
     function createLottery(
-        string bytes lotteryName, 
-        uint tokenId, 
-        uint startTime,
-        uint period
-    ) public onlyOwner returns(bool) {
-        require(block.timestamp < startTime, "Invalid start time");
-        require(period > 0, "Invalid period");
-        require(IYBNFT( HedgepieYBNFT ).chkToken(tokenId), "Token not existing");
+        bytes calldata _lotteryName,
+        uint256 _tokenId,
+        uint256 _startTime,
+        uint256 _period
+    ) public onlyOwner returns (bool) {
+        require(block.timestamp < _startTime, "Invalid start time");
+        require(_period > 0, "Invalid period");
+        require(IYBNFT(HedgepieYBNFT).chkToken(_tokenId), "Token not existing");
 
-        _createLottery(lotteryName, tokenId, startTime, period);
+        _createLottery(_lotteryName, _tokenId, _startTime, _period);
 
-        emit LotteryCreated(lotteryName, tokenId, startTime, period);
+        emit LotteryCreated(_lotteryName, _tokenId, _startTime, _period);
         return true;
     }
 
-    function deposit(uint lotteryId, uint amount) public {
-        require(amount > 0);
-        Lottery memory info = Lotteries[lotteryId];
-        require(info)
-        require(Lotteries[lotteryId].startTime > 0, "Invalid Lottery");
+    function deposit(uint256 _lotteryId, uint256 _amount) public {
+        require(_amount > 0);
+        Lottery memory info = Lotteries[_lotteryId];
+        require(Lotteries[_lotteryId].startTime > 0, "Invalid Lottery");
 
-        _deposit(lotteryId, amount);
+        _deposit(_lotteryId, _amount);
 
-        emit Deposit(msg.sender, lotteryId, amount);
+        emit Deposit(msg.sender, _lotteryId, _amount);
     }
 }
