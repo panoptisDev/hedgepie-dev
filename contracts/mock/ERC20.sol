0daff6bad8d8c52b51b9fd62dfdd0ebf146e7f84
daiki saito <daiki.saito92@gmail.com>
2022-02-11 00:58:49 +0900

    add mockERC20 contract for test purpose

---
+++ contracts/mock/ERC20.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _initialSupply
+    ) public ERC20(_name, _symbol) {
+        _mint(msg.sender, _initialSupply);
+    }
+}
