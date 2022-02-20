09dcc7cc6ce221ce80d9889bebbd81c21b4c44ce
daiki saito <daiki.saito92@gmail.com>
2022-02-21 01:17:46 +0900

    refactor YBNFT and Investor contracts: move deposit, withdraw, withdrawAll functions to Investor contract

--- contracts/HedgepieInvestor.sol
+++ contracts/HedgepieInvestor.sol
@@ -2,30 +2,198 @@
 pragma solidity ^0.8.4;
 
 import "./libraries/SafeBEP20.sol";
+import "./libraries/Ownable.sol";
 
-contract HedgepieInvestor {
+import "./interfaces/IYBNFT.sol";
+import "./interfaces/IPancakeRouter.sol";
+
+contract HedgepieInvestor is Ownable {
     using SafeBEP20 for IBEP20;
 
-    // user => token => amount
-    mapping(address => mapping(address => uint256)) public userStake;
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+    }
 
-    event Stake(address indexed user, address token, uint256 amount);
+    // user => nft address => nft id =>  UserInfo
+    mapping(address => mapping(address => mapping(uint256 => UserInfo)))
+        public userInfo;
+    // nft => listed status
+    mapping(address => bool) public nftWhiteList; // whitelisted nfts
 
-    receive() external payable {}
+    // wrapped bnb address
+    address public wbnb;
+    // pancakeswap router address
+    address public pancakeswapRouter;
 
-    constructor() {}
+    event Deposit(
+        address indexed user,
+        address nft,
+        uint256 nftId,
+        uint256 amount
+    );
+    event Withdraw(
+        address indexed user,
+        address nft,
+        uint256 nftId,
+        uint256 amount
+    );
+    event NftListed(address indexed user, address nft);
+    event NftDeListed(address indexed user, address nft);
+
+    /**
+     * @notice construct
+     * @param _pancakeswapRouter  Pancakeswap router address (0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3 // on bsc testnet)
+     * @param _wbnb  Wrapped BNB address (0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd // on bsc testnet)
+     */
+    constructor(address _pancakeswapRouter, address _wbnb) {
+        require(_pancakeswapRouter != address(0));
+        require(_wbnb != address(0));
+
+        pancakeswapRouter = _pancakeswapRouter;
+        wbnb = _wbnb;
+    }
+
+    // ===== modifiers =====
+    modifier onlyWhiteListedNft(address _nft) {
+        require(nftWhiteList[_nft], "Error: nft was not listed");
+        _;
+    }
 
     /**
      * @notice deposit fund
+     * @param _nft  YBNft address
+     * @param _tokenId  YBNft token id
      * @param _token  token address
      * @param _amount  token amount
      */
-    function deposit(address _token, uint256 _amount) public {
+    function deposit(
+        address _nft,
+        uint256 _tokenId,
+        address _token,
+        uint256 _amount
+    ) external onlyWhiteListedNft(msg.sender) {
         require(_amount > 0, "Amount can not be 0");
 
         IBEP20(_token).safeTransferFrom(msg.sender, address(this), _amount);
-        userStake[msg.sender][_token] += _amount;
 
-        emit Stake(msg.sender, _token, _amount);
+        // for token swap
+        IBEP20(_token).safeApprove(pancakeswapRouter, _amount);
+
+        // TODO: strategy handling // should think more
+        IYBNFT.Strategy[] memory info = IYBNFT(_nft).getNftStrategy(_tokenId);
+        for (uint8 idx = 0; idx < info.length; idx++) {
+            IYBNFT.Strategy memory infoItem = info[idx];
+
+            // swapping
+            uint256 amountIn = (_amount * infoItem.percent) / 1e4;
+            uint256 amountOut = _swapOnPCS(
+                amountIn,
+                _token,
+                infoItem.swapToken
+            );
+
+            // staking
+        }
+
+        UserInfo storage user = userInfo[msg.sender][_nft][_tokenId];
+        user.amount = user.amount + _amount;
+
+        emit Deposit(msg.sender, _nft, _tokenId, _amount);
+    }
+
+    /**
+     * @notice withdraw fund
+     * @param _nft  YBNft address
+     * @param _tokenId  YBNft token id
+     * @param _token  token address
+     * @param _amount  token amount
+     */
+    function withdraw(
+        address _nft,
+        uint256 _tokenId,
+        address _token,
+        uint256 _amount
+    ) external onlyWhiteListedNft(msg.sender) {
+        require(_amount > 0, "Amount can not be 0");
+
+        UserInfo storage user = userInfo[msg.sender][_nft][_tokenId];
+        require(user.amount > _amount, "Withdraw: exceeded amount");
+
+        IBEP20(_token).safeTransfer(msg.sender, _amount);
+
+        user.amount = user.amount - _amount;
+
+        emit Withdraw(msg.sender, _nft, _tokenId, _amount);
+    }
+
+    /**
+     * @notice withdraw fund
+     * @param _nft  YBNft address
+     * @param _tokenId  YBNft token id
+     * @param _token  token address
+     */
+    function withdrawAll(
+        address _nft,
+        uint256 _tokenId,
+        address _token
+    ) external onlyWhiteListedNft(msg.sender) {
+        UserInfo storage user = userInfo[msg.sender][_nft][_tokenId];
+        require(user.amount > 0, "Withdraw: amount is 0");
+
+        IBEP20(_token).safeTransfer(msg.sender, user.amount);
+
+        uint256 amount = user.amount;
+        user.amount = 0;
+
+        emit Withdraw(msg.sender, _nft, _tokenId, amount);
+    }
+
+    // ===== Owner functions =====
+    /**
+     * @notice list nft into whitelist
+     * @param _nft  nft address
+     */
+    function listNft(address _nft) external onlyOwner {
+        require(_nft != address(0), "Invalid NFT address");
+
+        nftWhiteList[_nft] = true;
+
+        emit NftListed(msg.sender, _nft);
+    }
+
+    /**
+     * @notice delist nft from whitelist
+     * @param _nft  nft address
+     */
+    function deListNft(address _nft) external onlyOwner {
+        require(_nft != address(0), "Invalid NFT address");
+
+        nftWhiteList[_nft] = false;
+
+        emit NftDeListed(msg.sender, _nft);
+    }
+
+    // TODO: ===== internal functions =====
+    function _swapOnPCS(
+        uint256 _amountIn,
+        address _inToken,
+        address _outToken
+    ) internal returns (uint256 amountOut) {
+        address[] memory path;
+        if (_outToken == wbnb || _inToken == wbnb) {
+            path = new address[](2);
+            path[0] = _inToken;
+            path[1] = _outToken;
+        } else {
+            path = new address[](3);
+            path[0] = _inToken;
+            path[1] = wbnb;
+            path[2] = _outToken;
+        }
+
+        uint256[] memory amounts = IPancakeRouter(pancakeswapRouter)
+            .swapExactTokensForTokens(_amountIn, 0, path, address(this), 0);
+        amountOut = amounts[amounts.length - 1];
     }
 }
--- contracts/YBNFT.sol
+++ contracts/YBNFT.sol
@@ -3,47 +3,44 @@
 pragma solidity ^0.8.4;
 
 import "./interfaces/IPancakeRouter.sol";
+import "./interfaces/IYBNFT.sol";
+import "./interfaces/IHedgepieInvestor.sol";
+
 import "./libraries/Ownable.sol";
 import "./libraries/SafeMath.sol";
 import "./libraries/SafeBEP20.sol";
+
 import "./type/BEP721.sol";
 
-contract YBNFT is BEP721, Ownable {
+contract YBNFT is BEP721, IYBNFT, Ownable {
     using SafeMath for uint256;
     using SafeBEP20 for IBEP20;
 
-    struct Strategy {
-        uint256 percent;
-        address swapToken;
-        address stakeAddress;
-    }
-
-    struct NFTFund {
-        address funder;
-        address token;
-        uint256 amount;
-    }
-
-    address public immutable lottery;
-    address public immutable treasury;
-
-    // these addresses are for testing
-    address public constant WBNB = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
-    address public constant PCSRouter =
-        0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3;
-
+    // lottery address
+    address public lottery;
+    // treasury address
+    address public treasury;
+    // investor address
+    address public investor;
+    // token => status
     mapping(address => bool) public allowedToken;
-    mapping(uint256 => Strategy[]) private nftStrategy;
-    mapping(uint256 => NFTFund[]) public NFTFunds;
+    // tokenId => strategy[]
+    mapping(uint256 => Strategy[]) nftStrategy;
 
-    // ===== events =====
     event Mint(uint256 indexed tokenId, address indexed user);
-    event Deposit(
-        uint256 indexed tokenId,
-        address indexed user,
-        address token,
-        uint256 amount
-    );
+
+    constructor(
+        address _investor,
+        address _lottery,
+        address _treasury
+    ) BEP721("Hedgepie YBNFT", "YBNFT") {
+        require(_investor != address(0));
+        require(_lottery != address(0));
+        require(_treasury != address(0));
+        investor = _investor;
+        lottery = _lottery;
+        treasury = _treasury;
+    }
 
     // ===== modifiers =====
     modifier onlyAllowedToken(address token) {
@@ -51,102 +48,18 @@ contract YBNFT is BEP721, Ownable {
         _;
     }
 
-    constructor(address _lottery, address _treasury)
-        BEP721("Hedgepie YBNFT", "YBNFT")
+    // TODO: ===== external functions =====
+    function getNftStrategy(uint256 _tokenId)
+        external
+        view
+        override
+        returns (Strategy[] memory)
     {
-        require(_lottery != address(0));
-        require(_treasury != address(0));
-
-        lottery = _lottery;
-        treasury = _treasury;
+        return nftStrategy[_tokenId];
     }
 
-    // ===== view functions =====
-    function chkToken(uint256 tokenId) external view returns (bool) {
-        return _exists(tokenId);
-    }
-
-    // ===== internal functions =====
-    function _setStrategy(
-        uint256 _tokenId,
-        uint256[] calldata _swapPercent,
-        address[] calldata _swapToken,
-        address[] calldata _stakeAddress
-    ) private {
-        for (uint8 idx = 0; idx < _swapToken.length; idx++) {
-            nftStrategy[_tokenId].push(
-                Strategy({
-                    percent: _swapPercent[idx],
-                    swapToken: _swapToken[idx],
-                    stakeAddress: _stakeAddress[idx]
-                })
-            );
-        }
-    }
-
-    function _deposit(
-        uint256 _tokenId,
-        address _token,
-        uint256 _amount
-    ) internal {
-        IBEP20(_token).safeTransferFrom(msg.sender, address(this), _amount);
-
-        // for token swap
-        IBEP20(_token).safeApprove(PCSRouter, _amount);
-
-        Strategy[] memory info = nftStrategy[_tokenId];
-        for (uint8 idx = 0; idx < info.length; idx++) {
-            Strategy memory infoItem = info[idx];
-
-            // swapping
-            uint256 amountIn = _amount.mul(infoItem.percent).div(1e4);
-            uint256 amountOut = _swapOnPCS(
-                amountIn,
-                _token,
-                infoItem.swapToken
-            );
-
-            // staking
-        }
-
-        NFTFunds[_tokenId].push(
-            NFTFund({funder: msg.sender, token: _token, amount: _amount})
-        );
-    }
-
-    function _checkPercent(uint256[] calldata _swapPercent)
-        internal
-        pure
-        returns (bool)
-    {
-        uint256 totalPercent;
-        for (uint256 idx = 0; idx < _swapPercent.length; idx++) {
-            totalPercent = totalPercent.add(_swapPercent[idx]);
-        }
-
-        return totalPercent.sub(1e4) == 0;
-    }
-
-    function _swapOnPCS(
-        uint256 _amountIn,
-        address _inToken,
-        address _outToken
-    ) internal returns (uint256 amountOut) {
-        address[] memory path;
-        if (_outToken == WBNB || _inToken == WBNB) {
-            path = new address[](2);
-            path[0] = _inToken;
-            path[1] = _outToken;
-        } else {
-            path = new address[](3);
-            path[0] = _inToken;
-            path[1] = WBNB;
-            path[2] = _outToken;
-        }
-
-        uint256[] memory amounts = IPancakeRouter(PCSRouter)
-            .swapExactTokensForTokens(_amountIn, 0, path, address(this), 0);
-        amountOut = amounts[amounts.length - 1];
+    function chkToken(uint256 _tokenId) external view returns (bool) {
+        return _exists(_tokenId);
     }
 
     // external functions
@@ -175,6 +88,7 @@ contract YBNFT is BEP721, Ownable {
         emit Mint(_tokenId, address(this));
     }
 
+    // TODO: ===== public functions =====
     function manageToken(address[] calldata _tokens, bool _flag)
         public
         onlyOwner
@@ -188,14 +102,98 @@ contract YBNFT is BEP721, Ownable {
 
     function deposit(
         uint256 _tokenId,
-        uint256 _amount,
-        address _token
+        address _token,
+        uint256 _amount
     ) external onlyAllowedToken(_token) {
         require(_exists(_tokenId), "BEP721: NFT not exist");
         require(_amount > 0);
 
         _deposit(_tokenId, _token, _amount);
+    }
 
-        emit Deposit(_tokenId, msg.sender, _token, _amount);
+    function withdraw(
+        uint256 _tokenId,
+        address _token,
+        uint256 _amount
+    ) external onlyAllowedToken(_token) {
+        require(_exists(_tokenId), "BEP721: NFT not exist");
+        require(_amount > 0);
+
+        _withdraw(_tokenId, _token, _amount);
+    }
+
+    function withdraw(uint256 _tokenId, address _token)
+        external
+        onlyAllowedToken(_token)
+    {
+        require(_exists(_tokenId), "BEP721: NFT not exist");
+
+        _withdrawAll(_tokenId, _token);
+    }
+
+    // TODO: ===== internal functions =====
+    function _setStrategy(
+        uint256 _tokenId,
+        uint256[] calldata _swapPercent,
+        address[] calldata _swapToken,
+        address[] calldata _stakeAddress
+    ) internal {
+        for (uint8 idx = 0; idx < _swapToken.length; idx++) {
+            nftStrategy[_tokenId].push(
+                Strategy({
+                    percent: _swapPercent[idx],
+                    swapToken: _swapToken[idx],
+                    stakeAddress: _stakeAddress[idx]
+                })
+            );
+        }
+    }
+
+    function _deposit(
+        uint256 _tokenId,
+        address _token,
+        uint256 _amount
+    ) internal {
+        IBEP20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+        IHedgepieInvestor(investor).deposit(
+            address(this),
+            _tokenId,
+            _token,
+            _amount
+        );
+    }
+
+    function _withdraw(
+        uint256 _tokenId,
+        address _token,
+        uint256 _amount
+    ) internal {
+        IHedgepieInvestor(investor).withdraw(
+            address(this),
+            _tokenId,
+            _token,
+            _amount
+        );
+    }
+
+    function _withdrawAll(uint256 _tokenId, address _token) internal {
+        IHedgepieInvestor(investor).withdrawAll(
+            address(this),
+            _tokenId,
+            _token
+        );
+    }
+
+    function _checkPercent(uint256[] calldata _swapPercent)
+        internal
+        pure
+        returns (bool)
+    {
+        uint256 totalPercent;
+        for (uint256 idx = 0; idx < _swapPercent.length; idx++) {
+            totalPercent = totalPercent.add(_swapPercent[idx]);
+        }
+
+        return totalPercent.sub(1e4) == 0;
     }
 }
