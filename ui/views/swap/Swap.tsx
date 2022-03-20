a949ce99bfc692255f514e1fe7c972e2225fea5a
S Deepak Srinivas <87278119+sdscode00@users.noreply.github.com>
2022-03-20 17:21:26 +0530

    Review Comments Addressed

--- ui/views/swap/Swap.tsx
+++ ui/views/swap/Swap.tsx
@@ -2,114 +2,48 @@
 /** @jsxRuntime classic */
 
 import { useState, useEffect } from 'react'
-import { ThemeProvider, jsx, Box, Flex, Button, Text, Image, Input, Select } from 'theme-ui'
+import { ThemeProvider, jsx, Box, Flex, Button, Text, Image, Input, Select, ThemeUICSSObject } from 'theme-ui'
 
 import { theme } from 'themes/theme'
 import { VscSettings } from 'react-icons/vsc'
 import { AiOutlineArrowRight } from 'react-icons/ai'
+
+import { styles } from './styles'
 
 type Props = {}
 
 const Swap = (props: Props) => {
   const [swapTokenType, setSwapTokenType] = useState('')
   const [tokenQuantity, setTokenQuantity] = useState(0)
-  const [HPIEQuantity, setHPIEQuantity] = useState(0)
+  const [hpieQuantity, setHpieQuantity] = useState(0)
   const [slippageTolerance, setSlippageTolerance] = useState(0)
 
   // Function called on clicking Swap
   const swap = () => {
     console.log('Swap Token Type : ' + swapTokenType)
     console.log('Swap Token Quantity : ' + tokenQuantity)
-    console.log('HPIE Quantity : ' + HPIEQuantity)
+    console.log('HPIE Quantity : ' + hpieQuantity)
     console.log('Slippage Tolerance : ' + slippageTolerance)
   }
 
   return (
     <ThemeProvider theme={theme}>
       <Box p={3}>
-        <Flex
-          sx={{
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Flex
-            sx={{
-              width: '75rem',
-              marginTop: '3rem',
-              boxShadow: '0px 25px 55px rgba(209, 208, 219, 0.4)',
-              flexDirection: 'column',
-            }}
-          >
-            <Flex
-              sx={{
-                height: '5rem',
-                alignItems: 'center',
-                paddingLeft: '2rem',
-                backgroundColor: '#1799DE',
-                color: '#fff',
-                fontWeight: '600',
-                fontFamily: 'Poppins',
-                fontSize: '26px',
-              }}
-            >
-              Swap for HPIE
-            </Flex>
-            <Flex sx={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-              <Flex
-                sx={{
-                  margin: '1rem',
-                  flexDirection: 'column',
-                  backgroundColor: '#E5F6FF',
-                  borderRadius: '6px',
-                  width: '80%',
-                  padding: '0rem 3rem',
-                  paddingBottom: '5rem',
-                }}
-              >
-                <Flex sx={{ flexDirection: 'row', marginBottom: '1rem', marginTop: '1rem' }}>
-                  <Text
-                    sx={{
-                      fontFamily: 'Noto Sans',
-                      fontWeight: '600',
-                      fontSize: '16px',
-                      color: '#DF4886',
-                    }}
-                  >
-                    Trade tokens instantly for HPIE
-                  </Text>
-                  <VscSettings
-                    sx={{
-                      color: '#1799DE',
-                      width: '1.5rem',
-                      height: '1.5rem',
-                      marginLeft: 'auto',
-                      marginRight: '2rem',
-                    }}
-                  />
+        <Flex sx={styles.flex_outer as ThemeUICSSObject}>
+          <Flex sx={styles.flex_container as ThemeUICSSObject}>
+            <Flex sx={styles.title as ThemeUICSSObject}>Swap for HPIE</Flex>
+            <Flex sx={styles.flex_column as ThemeUICSSObject}>
+              <Flex sx={styles.flex_inner_container as ThemeUICSSObject}>
+                <Flex sx={styles.flex_sub_title as ThemeUICSSObject}>
+                  <Text sx={styles.sub_title_text as ThemeUICSSObject}>Trade tokens instantly for HPIE</Text>
+                  <VscSettings sx={styles.filter_icon as ThemeUICSSObject} />
                 </Flex>
-                <Flex sx={{ gap: '1rem', alignItems: 'center', marginBottom: '1rem' }}>
-                  <Image src="images/tako.svg" sx={{ width: '50px', height: '50px' }} />
+                <Flex sx={styles.flex_swap_details as ThemeUICSSObject}>
+                  <Image src="images/tako.svg" sx={styles.swap_token_image as ThemeUICSSObject} />
                   <Select
                     defaultValue="Type"
                     onChange={(e) => setSwapTokenType(e.target.value)}
-                    sx={{
-                      borderRadius: '10rem',
-                      outline: 'none',
-                      border: 0,
-                      height: '50px',
-                      paddingInlineEnd: '24px',
-                      flex: 1,
-                      width: '6rem',
-                      fontSize: '22px',
-                      fontWeight: '600',
-                      color: '#0A3F5C',
-                      marginBottom: '2px',
-                      appearance: 'none',
-                      WebkitAppearance: 'none',
-                      marginTop: '5px',
-                      paddingLeft: '10px',
-                    }}
+                    sx={styles.swap_token_select as ThemeUICSSObject}
                   >
                     <option>Tako</option>
                     <option>Option 2</option>
@@ -121,119 +55,36 @@ const Swap = (props: Props) => {
                     onChange={(e) => {
                       setTokenQuantity(Number(e.target.value))
                     }}
-                    sx={{
-                      borderRadius: '30px',
-                      width: '15rem',
-                      boxShadow: 'none',
-                      border: 'none',
-                      outline: 0,
-                      paddingLeft: '1rem',
-                      color: '#0A3F5C',
-                      backgroundColor: '#fff',
-                      fontSize: '30px',
-                      fontWeight: '700',
-                      textAlign: 'right',
-                      paddingRight: '1.5rem',
-                    }}
+                    sx={styles.swap_token_input as ThemeUICSSObject}
                   />
 
-                  <Flex
-                    sx={{
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      backgroundColor: '#DF4886',
-                      padding: '0.3rem',
-                      borderRadius: '50px',
-                    }}
-                  >
-                    <AiOutlineArrowRight sx={{ width: '2rem', height: '2rem', color: '#fff' }} />
+                  <Flex sx={styles.flex_arrow_container as ThemeUICSSObject}>
+                    <AiOutlineArrowRight sx={styles.arrow_icon as ThemeUICSSObject} />
                   </Flex>
-                  <Image src="images/logo.png" sx={{ width: '60px', height: '50px' }} />
-                  <Select
-                    defaultValue="HPIE"
-                    sx={{
-                      borderRadius: '10rem',
-                      outline: 'none',
-                      border: 0,
-                      height: '50px',
-                      paddingInlineEnd: '24px',
-                      flex: 1,
-                      width: '6rem',
-                      fontSize: '22px',
-                      fontWeight: '600',
-                      color: '#0A3F5C',
-                      marginBottom: '2px',
-                      appearance: 'none',
-                      WebkitAppearance: 'none',
-                      marginTop: '5px',
-                      paddingLeft: '10px',
-                    }}
-                  >
+                  <Image src="images/logo.png" sx={styles.hpie_token_image as ThemeUICSSObject} />
+                  <Select defaultValue="HPIE" sx={styles.swap_token_select as ThemeUICSSObject}>
                     <option>HPIE</option>
                   </Select>
                   <Input
                     defaultValue={'1.9'}
                     onChange={(e) => {
-                      setHPIEQuantity(Number(e.target.value))
+                      setHpieQuantity(Number(e.target.value))
                     }}
-                    sx={{
-                      borderRadius: '30px',
-                      width: '15rem',
-                      boxShadow: 'none',
-                      border: 'none',
-                      outline: 0,
-                      paddingLeft: '1rem',
-                      color: '#0A3F5C',
-                      backgroundColor: '#fff',
-                      fontSize: '30px',
-                      fontWeight: '700',
-                      textAlign: 'right',
-                      paddingRight: '1.5rem',
-                    }}
+                    sx={styles.swap_token_input as ThemeUICSSObject}
                   />
                 </Flex>
-                <Flex sx={{ marginLeft: '55%', alignItems: 'center', gap: '0.5rem' }}>
-                  <Text sx={{ color: '#8E8DA0', fontWeight: '600', fontSize: '16px', fontFamily: 'Noto Sans' }}>
-                    Slippage Tolerance:
-                  </Text>
+                <Flex sx={styles.flex_slippage_tolerance as ThemeUICSSObject}>
+                  <Text sx={styles.slippage_tolerance_text as ThemeUICSSObject}>Slippage Tolerance:</Text>
                   <Input
                     defaultValue={'0.5%'}
                     onChange={(e) => {
                       setSlippageTolerance(Number(e.target.value))
                     }}
-                    sx={{
-                      borderRadius: '30px',
-                      width: '5rem',
-                      boxShadow: 'none',
-                      border: 'none',
-                      outline: 0,
-                      paddingLeft: '1rem',
-                      color: '#EFA906',
-                      backgroundColor: '#fff',
-                      fontSize: '16px',
-                      fontWeight: '700',
-                      textAlign: 'center',
-                      paddingRight: '1.5rem',
-                    }}
+                    sx={styles.slippage_tolerance_input as ThemeUICSSObject}
                   />
                 </Flex>
               </Flex>
-              <Button
-                sx={{
-                  margin: '1rem',
-                  marginBottom: '4rem',
-                  cursor: 'pointer',
-                  backgroundColor: '#1799DE',
-                  color: '#fff',
-                  fontSize: '16px',
-                  fontWeight: '600',
-                  height: '4rem',
-                  width: '50%',
-                  borderRadius: '30px',
-                  letterSpacing: '0.2rem',
-                }}
-                onClick={swap}
-              >
+              <Button sx={styles.swap_button as ThemeUICSSObject} onClick={swap}>
                 SWAP
               </Button>
             </Flex>
