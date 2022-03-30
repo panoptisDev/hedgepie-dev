cb89c2abf5e0f638734e37a9d74ca7d4641a74b4
daiki saito <daiki.saito92@gmail.com>
2022-03-31 00:25:51 +0900

    fix undefined issue

--- ui/utils/addressHelpers.ts
+++ ui/utils/addressHelpers.ts
@@ -2,8 +2,8 @@ import addresses from 'config/constants/contracts'
 import { Address } from 'config/constants/types'
 
 export const getAddress = (address: Address): string => {
-  const mainNetChainId = 97
-  const chainId = process.env.REACT_APP_CHAIN_ID
+  const mainNetChainId = 56
+  const chainId = 97
   return address[chainId] ? address[chainId] : address[mainNetChainId]
 }
 
