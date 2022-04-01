85eeeb50864f151ffefd6ead7507f8a1a2495643
daiki saito <daiki.saito92@gmail.com>
2022-04-02 01:34:33 +0900

    add rewardHolder for reward distribution

--- v1-contract/config/construct-arguments.ts
+++ v1-contract/config/construct-arguments.ts
@@ -3,6 +3,7 @@ export const masterChefArgs = {
   lp: "0x3CdFF37d36278e13e1f68f6A0faECcDaB699E264", // HPIE token address
   rewardToken: "0x3CdFF37d36278e13e1f68f6A0faECcDaB699E264", // HPIE token address
   rewardPerBlock: "5000000000000000000", // 5 per block
+  rewardHolder: "0x61B2E3aC6E5d7712537ed0909eed1ee838b410D3"
 };
 
 export const mockBEP20Args = {
