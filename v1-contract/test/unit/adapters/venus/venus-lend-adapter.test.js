8343f00b64eabeccb5fad3a8a0047c82925a2bc4
0xmainnov <mauro.liu@innovationupstream.com>
2022-05-15 23:04:26 +0800

    New unit-tests of venus adaptor

--- v1-contract/test/unit/adapters/venus/venus-lend-adapter.test.js
+++ v1-contract/test/unit/adapters/venus/venus-lend-adapter.test.js
@@ -1,75 +1,86 @@
 const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("VenusAdapter Fuzz Test", function () {
+describe("VenusAdapter Unit Test", function () {
   before("Deploy contract", async function () {
-    const [owner, alice] = await ethers.getSigners();
+    const [owner, investor, alice] = await ethers.getSigners();
 
-    this.owner = owner;
     this.alice = alice;
-    this.rewardFee = 500;
-    this.vBUSD = "0x95c78222B3D6e262426483D42CfA53685A67Ab9D";
-    this.BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+    this.owner = owner;
+    this.investor = investor;
+    this.strategy = "0x95c78222B3D6e262426483D42CfA53685A67Ab9D";
 
     // Deploy Venus Adapter contract
-    const VenusAdapter = await ethers.getContractFactory("VenusAdapterMock");
-    this.vAdapter = await VenusAdapter.deploy(this.rewardFee);
+    const VenusAdapter = await ethers.getContractFactory("VenusLendAdapter");
+    this.vAdapter = await VenusAdapter.deploy(
+      this.strategy,
+      this.investor.address
+    );
     await this.vAdapter.deployed();
 
     console.log("Owner: ", this.owner.address);
+    console.log("Investor: ", this.investor.address);
+    console.log("Strategy: ", this.strategy);
     console.log("VenusAdapter: ", this.vAdapter.address);
   });
 
   describe("should set correct state variable", function () {
-    it("(1) Check reward fee", async function () {
-      expect(await this.vAdapter.rewardFee()).to.eq(this.rewardFee);
+    it("(1) Check investor address", async function () {
+      expect(await this.vAdapter.investor()).to.eq(this.investor.address);
     });
 
-    it("(2) Check owner address", async function () {
+    it("(2) Check strategy address", async function () {
+      expect(await this.vAdapter.strategy()).to.eq(this.strategy);
+    });
+
+    it("(3) Check owner wallet", async function () {
       expect(await this.vAdapter.owner()).to.eq(this.owner.address);
-    });
-
-    it("(3) Check pausable state", async function () {
-      expect(await this.vAdapter.paused()).to.eq(false);
     });
   });
 
-  describe("should adding vTokens properly", function () {
-    it("revert addVTokens not from owner", async function () {
+  describe("should get invest call data only from investor", function () {
+    it("revert not from investor", async function () {
       await expect(
-        this.vAdapter.connect(this.alice).addVTokens([this.vBUSD], [this.BUSD])
+        this.vAdapter.connect(this.alice).getInvestCallData("0")
+      ).to.be.revertedWith("Error: caller is not investor");
+    });
+
+    it("should get invest call data properly", async function () {
+      const result = await this.vAdapter
+        .connect(this.investor)
+        .getInvestCallData("0");
+
+      expect(!!result).to.eq(true);
+    });
+  });
+
+  describe("should get devest call data only from investor", function () {
+    it("revert not from investor", async function () {
+      await expect(
+        this.vAdapter.connect(this.alice).getDevestCallData("0")
+      ).to.be.revertedWith("Error: caller is not investor");
+    });
+
+    it("should get devest call data properly", async function () {
+      const result = await this.vAdapter
+        .connect(this.investor)
+        .getDevestCallData("0");
+
+      expect(!!result).to.eq(true);
+    });
+  });
+
+  describe("should set investor only from owner wallet", function () {
+    it("revert when from owner wallet", async function () {
+      await expect(
+        this.vAdapter.connect(this.alice).setInvestor(this.owner.address)
       ).to.be.revertedWith("Ownable: caller is not the owner");
     });
 
-    it("addVToken set state properly", async function () {
-      await this.vAdapter.addVTokens([this.vBUSD], [this.BUSD]);
+    it("test the investor address correctly", async function () {
+      await this.vAdapter.setInvestor(this.owner.address);
 
-      expect(await this.vAdapter.isVToken(this.vBUSD)).to.eq(true) &&
-        expect(await this.vAdapter.nTokens(this.vBUSD)).to.eq(this.BUSD) &&
-        expect(await this.vAdapter.vTokens(this.BUSD)).to.eq(this.vBUSD);
-    });
-  });
-
-  describe("should pause supply & redeem", function () {
-    it("pause contract", async function () {
-      await this.vAdapter.pause();
-
-      expect(await this.vAdapter.paused()).to.eq(true);
-    });
-
-    it("test paused supply method", async function () {
-      await expect(
-        this.vAdapter.supply(this.BUSD, ethers.utils.parseEther("1").toString())
-      ).to.be.revertedWith("Pausable: paused");
-    });
-
-    it("test paused redeem method", async function () {
-      await expect(
-        this.vAdapter.redeem(
-          this.vBUSD,
-          ethers.utils.parseEther("1").toString()
-        )
-      ).to.be.revertedWith("Pausable: paused");
+      expect(await this.vAdapter.investor()).to.eq(this.owner.address);
     });
   });
 });
