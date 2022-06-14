30853b0129ae89f03f86225b1f7f806c8e8ea406
daiki-iu <daiki.saito@innovationupstream.com>
2022-06-14 23:35:14 +0900

    update getPaths function of apeswap lp contract

--- v1-contract/contracts/adapters/apeswap/apeswap-lp-adapter.sol
+++ v1-contract/contracts/adapters/apeswap/apeswap-lp-adapter.sol
@@ -55,70 +55,6 @@ contract ApeswapLPAdapter is Ownable {
     }
 
     /**
-     * @notice Get path
-     * @param _inToken token address of inToken
-     * @param _outToken token address of outToken
-     */
-    function getPaths(address _inToken, address _outToken)
-        external
-        view
-        onlyInvestor
-        returns (address[] memory)
-    {
-        require(
-            paths[_inToken][_outToken].length > 1,
-            "Path length is not valid"
-        );
-        require(
-            paths[_inToken][_outToken][0] == _inToken,
-            "Path is not existed"
-        );
-        require(
-            paths[_inToken][_outToken][paths[_inToken][_outToken].length - 1] ==
-                _inToken,
-            "Path is not existed"
-        );
-
-        return paths[_inToken][_outToken];
-    }
-
-    /**
-     * @notice Set paths from inToken to outToken
-     * @param _inToken token address of inToken
-     * @param _outToken token address of outToken
-     * @param _paths swapping paths
-     */
-    function setPath(
-        address _inToken,
-        address _outToken,
-        address[] memory _paths
-    ) external onlyOwner {
-        require(_paths.length > 1, "Invalid paths length");
-        require(_inToken == _paths[0], "Invalid inToken address");
-        require(
-            _outToken == _paths[_paths.length - 1],
-            "Invalid inToken address"
-        );
-
-        uint8 i;
-
-        for (i = 0; i < _paths.length; i++) {
-            if (i < paths[_inToken][_outToken].length) {
-                paths[_inToken][_outToken][i] = _paths[i];
-            } else {
-                paths[_inToken][_outToken].push(_paths[i]);
-            }
-        }
-
-        if (paths[_inToken][_outToken].length > _paths.length)
-            for (
-                i = 0;
-                i < paths[_inToken][_outToken].length - _paths.length;
-                i++
-            ) paths[_inToken][_outToken].pop();
-    }
-
-    /**
      * @notice Get withdrwal amount
      * @param _user  user address
      * @param _nftId  nftId
@@ -210,6 +146,70 @@ contract ApeswapLPAdapter is Ownable {
     function setInvestor(address _investor) external onlyOwner {
         require(_investor != address(0), "Error: Investor zero address");
         investor = _investor;
+    }
+
+    /**
+     * @notice Get path
+     * @param _inToken token address of inToken
+     * @param _outToken token address of outToken
+     */
+    function getPaths(address _inToken, address _outToken)
+        external
+        view
+        onlyInvestor
+        returns (address[] memory)
+    {
+        require(
+            paths[_inToken][_outToken].length > 1,
+            "Path length is not valid"
+        );
+        require(
+            paths[_inToken][_outToken][0] == _inToken,
+            "Path is not existed"
+        );
+        require(
+            paths[_inToken][_outToken][paths[_inToken][_outToken].length - 1] ==
+                _outToken,
+            "Path is not existed"
+        );
+
+        return paths[_inToken][_outToken];
+    }
+
+    /**
+     * @notice Set paths from inToken to outToken
+     * @param _inToken token address of inToken
+     * @param _outToken token address of outToken
+     * @param _paths swapping paths
+     */
+    function setPath(
+        address _inToken,
+        address _outToken,
+        address[] memory _paths
+    ) external onlyOwner {
+        require(_paths.length > 1, "Invalid paths length");
+        require(_inToken == _paths[0], "Invalid inToken address");
+        require(
+            _outToken == _paths[_paths.length - 1],
+            "Invalid inToken address"
+        );
+
+        uint8 i;
+
+        for (i = 0; i < _paths.length; i++) {
+            if (i < paths[_inToken][_outToken].length) {
+                paths[_inToken][_outToken][i] = _paths[i];
+            } else {
+                paths[_inToken][_outToken].push(_paths[i]);
+            }
+        }
+
+        if (paths[_inToken][_outToken].length > _paths.length)
+            for (
+                i = 0;
+                i < paths[_inToken][_outToken].length - _paths.length;
+                i++
+            ) paths[_inToken][_outToken].pop();
     }
 
     /**
