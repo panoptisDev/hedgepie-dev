eafb849f2bd058b2bc5dda62e75197b08c37d2dc
0xmainnov <mauro.liu@innovationupstream.com>
2022-07-13 11:23:18 +0800

    Init short leverage adapter contract

---
+++ v1-contract/contracts/adapters/venus/venus-short-lev-adapter.sol
@@ -0,0 +1,247 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./interface/VBep20Interface.sol";
+import "./interface/ComptrollerInterface.sol";
+
+contract VenusShortLevAdapter is Ownable {
+    uint256 pid;
+    address public stakingToken;
+    address public rewardToken;
+    address public repayToken;
+    address public strategy;
+    address public vStrategy;
+    address public router;
+    string public name;
+    address public investor;
+
+    // inToken => outToken => paths
+    mapping(address => mapping(address => address[])) public paths;
+
+    // user => nft id => withdrawal amount
+    mapping(address => mapping(uint256 => uint256)) public withdrawalAmount;
+
+    modifier onlyInvestor() {
+        require(msg.sender == investor, "Error: Caller is not investor");
+        _;
+    }
+
+    /**
+     * @notice Construct
+     * @param _strategy  address of strategy
+     * @param _stakingToken  address of staking token
+     * @param _rewardToken  address of reward token
+     * @param _name  adatper name
+     */
+    constructor(address _strategy, string memory _name) {
+        require(
+            VBep20Interface(_strategy).isVToken(),
+            "Error: Invalid vToken address"
+        );
+        require(
+            VBep20Interface(_strategy).underlying() != address(0),
+            "Error: Invalid underlying address"
+        );
+
+        stakingToken = VBep20Interface(_strategy).underlying();
+        rewardToken = _strategy;
+        strategy = _strategy;
+        router = _router;
+        name = _name;
+    }
+
+    /**
+     * @notice Get withdrwal amount
+     * @param _user  user address
+     * @param _nftId  nftId
+     */
+    function getWithdrawalAmount(address _user, uint256 _nftId)
+        external
+        view
+        returns (uint256 amount)
+    {
+        amount = withdrawalAmount[_user][_nftId];
+    }
+
+    /**
+     * @notice Get enter market calldata
+     */
+    function getEnterMarketCallData()
+        external
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data
+        )
+    {
+        to = VBep20Interface(strategy).comptroller();
+        value = 0;
+        data = abi.encodeWithSignature("enterMarkets(address[])", [strategy]);
+    }
+
+    /**
+     * @notice Get loan market calldata
+     * @param _amount  amount of loan
+     */
+    function getLoanCallData(uint256 _amount)
+        external
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data
+        )
+    {
+        to = strategy;
+        value = 0;
+        data = abi.encodeWithSignature("borrow(uint256)", _amount);
+    }
+
+    /**
+     * @notice Get invest calldata
+     * @param _amount  amount of invest
+     */
+    function getInvestCallData(uint256 _amount)
+        external
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data
+        )
+    {
+        to = strategy;
+        value = 0;
+        data = abi.encodeWithSignature("mint(uint256)", _amount);
+    }
+
+    /**
+     * @notice Get devest calldata
+     * @param _amount  amount of devest
+     */
+    function getDevestCallData(uint256 _amount)
+        external
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data
+        )
+    {
+        to = strategy;
+        value = 0;
+        data = abi.encodeWithSignature("redeemUnderlying(uint256)", _amount);
+    }
+
+    /**
+     * @notice Increase withdrwal amount
+     * @param _user  user address
+     * @param _nftId  nftId
+     * @param _amount  amount of withdrawal
+     */
+    function increaseWithdrawalAmount(
+        address _user,
+        uint256 _nftId,
+        uint256 _amount
+    ) external onlyInvestor {
+        withdrawalAmount[_user][_nftId] += _amount;
+    }
+
+    /**
+     * @notice Set withdrwal amount
+     * @param _user  user address
+     * @param _nftId  nftId
+     * @param _amount  amount of withdrawal
+     */
+    function setWithdrawalAmount(
+        address _user,
+        uint256 _nftId,
+        uint256 _amount
+    ) external onlyInvestor {
+        withdrawalAmount[_user][_nftId] = _amount;
+    }
+
+    /**
+     * @notice Set investor
+     * @param _investor  address of investor
+     */
+    function setInvestor(address _investor) external onlyOwner {
+        require(_investor != address(0), "Error: Investor zero address");
+        investor = _investor;
+    }
+
+    /**
+     * @notice Get path
+     * @param _inToken token address of inToken
+     * @param _outToken token address of outToken
+     */
+    function getPaths(address _inToken, address _outToken)
+        external
+        view
+        onlyInvestor
+        returns (address[] memory)
+    {
+        require(
+            paths[_inToken][_outToken].length > 1,
+            "Path length is not valid"
+        );
+        require(
+            paths[_inToken][_outToken][0] == _inToken,
+            "Path is not existed"
+        );
+        require(
+            paths[_inToken][_outToken][paths[_inToken][_outToken].length - 1] ==
+                _outToken,
+            "Path is not existed"
+        );
+
+        return paths[_inToken][_outToken];
+    }
+
+    /**
+     * @notice Set paths from inToken to outToken
+     * @param _inToken token address of inToken
+     * @param _outToken token address of outToken
+     * @param _paths swapping paths
+     */
+    function setPath(
+        address _inToken,
+        address _outToken,
+        address[] memory _paths
+    ) external onlyOwner {
+        require(_paths.length > 1, "Invalid paths length");
+        require(_inToken == _paths[0], "Invalid inToken address");
+        require(
+            _outToken == _paths[_paths.length - 1],
+            "Invalid inToken address"
+        );
+
+        uint8 i;
+
+        for (i = 0; i < _paths.length; i++) {
+            if (i < paths[_inToken][_outToken].length) {
+                paths[_inToken][_outToken][i] = _paths[i];
+            } else {
+                paths[_inToken][_outToken].push(_paths[i]);
+            }
+        }
+
+        if (paths[_inToken][_outToken].length > _paths.length)
+            for (
+                i = 0;
+                i < paths[_inToken][_outToken].length - _paths.length;
+                i++
+            ) paths[_inToken][_outToken].pop();
+    }
+
+    /**
+     * @notice Get pending reward
+     * @param _user  address of investor
+     */
+    function getReward(address _user) external view returns (uint256) {
+        return IStrategy(strategy).pendingBSW(pid, _user);
+    }
+}
