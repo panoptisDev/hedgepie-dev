7ea53419a2582d9d015af7814105e4e915c1b028
abe.sinjiro <abe.sinjiro7@gmail.com>
2022-08-19 08:49:53 +0900

    adding adapter contracts

---
+++ v1-contract/contracts/adapters/polygon/quickswap/quick-lp-dual-adapter.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "../../BaseAdapterMatic.sol";
+
+contract QuickLPDualAdapter is BaseAdapterMatic {
+    /**
+     * @notice Construct
+     * @param _strategy  address of strategy
+     * @param _stakingToken  address of staking token
+     * @param _rewardToken  address of 1st reward token
+     * @param _rewardToken1  address of 2nd reward token
+     * @param _router  address of reward token
+     * @param _name  adatper name
+     */
+    constructor(
+        address _strategy,
+        address _stakingToken,
+        address _rewardToken,
+        address _rewardToken1,
+        address _router,
+        string memory _name
+    ) {
+        stakingToken = _stakingToken;
+        rewardToken = _rewardToken;
+        rewardToken1 = _rewardToken1;
+        router = _router;
+        strategy = _strategy;
+        name = _name;
+
+        isReward = true;
+    }
+    
+    /**
+     * @notice Get invest calldata
+     * @param _amount  amount of invest
+     */
+    function getInvestCallData(uint256 _amount)
+        external
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data
+        )
+    {
+        to = strategy;
+        value = 0;
+        data = abi.encodeWithSignature("stake(uint256)", _amount);
+    }
+
+    /**
+     * @notice Get devest calldata
+     * @param _amount  amount of devest
+     */
+    function getDevestCallData(uint256 _amount)
+        external
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data
+        )
+    {
+        to = strategy;
+        value = 0;
+        data = abi.encodeWithSignature("withdraw(uint256)", _amount);
+    }
+
+    /**
+     * @notice Get reward calldata
+     */
+    function getRewardCallData()
+        external
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data
+        )
+    {
+        to = strategy;
+        value = 0;
+        data = abi.encodeWithSignature("getReward()");
+    }
+}
