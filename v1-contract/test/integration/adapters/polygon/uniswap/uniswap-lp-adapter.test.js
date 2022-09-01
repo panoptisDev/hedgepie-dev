be5ab9da120c593f40007861caa4cd0f0c1dd98b
abe.sinjiro <abe.sinjiro7@gmail.com>
2022-09-01 23:37:41 +0900

    remove only from testing file

--- v1-contract/test/integration/adapters/polygon/uniswap/uniswap-lp-adapter.test.js
+++ v1-contract/test/integration/adapters/polygon/uniswap/uniswap-lp-adapter.test.js
@@ -16,7 +16,7 @@ const forkNetwork = async () => {
   })
 }
 
-describe.only("UniswapLPAdapter Integration Test", function () {
+describe("UniswapLPAdapter Integration Test", function () {
     before("Deploy contract", async function () {
         await forkNetwork();
 
