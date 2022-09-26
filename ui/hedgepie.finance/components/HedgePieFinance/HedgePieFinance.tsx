30b4dd9ef2e6e378169e4e9f7c524dd68f747d6a
S Deepak Srinivas <sdscode00@gmail.com>
2022-09-26 13:51:35 +0530

    Alignment Changes

--- ui/hedgepie.finance/components/HedgePieFinance/HedgePieFinance.tsx
+++ ui/hedgepie.finance/components/HedgePieFinance/HedgePieFinance.tsx
@@ -21,43 +21,45 @@ const HedgePieFinance = (props: Props) => {
   const { title, children, isV2 } = props
   return (
     <ThemeProvider theme={theme}>
-      <Header dark={props.dark} overlay={props.overlayHeader} />
-      {!isV2 && (
-        <Image
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            objectPosition: 'right',
-            objectFit: 'cover',
-            zIndex: -1,
-          }}
-          src="/images/backdesign.svg"
+      <Box sx={{ backgroundColor: '#F6F7FB' }}>
+        <Header dark={props.dark} overlay={props.overlayHeader} />
+        {!isV2 && (
+          <Image
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              objectPosition: 'right',
+              objectFit: 'cover',
+              zIndex: -1,
+            }}
+            src="/images/backdesign.svg"
+          />
+        )}
+        {title && <TitleMast title={title} />}
+        <Head>
+          <title>Hedge Pie</title>
+          <meta name="description" content="Hedge Pie Finance" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        {children}
+        <ToastContainer
+          position="bottom-right"
+          autoClose={2000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          icon={false}
+          closeButton={true}
         />
-      )}
-      {title && <TitleMast title={title} />}
-      <Head>
-        <title>Hedge Pie</title>
-        <meta name="description" content="Hedge Pie Finance" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {children}
-      <ToastContainer
-        position="bottom-right"
-        autoClose={2000}
-        hideProgressBar={true}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        icon={false}
-        closeButton={true}
-      />
-      <Footer />
+        <Footer />
+      </Box>
     </ThemeProvider>
   )
 }
