bbec02efc281d3d8f564c14373711968cf54564d
S Deepak Srinivas <sdscode00@gmail.com>
2022-09-27 19:42:40 +0530

    My Funds section

--- ui/hedgepie.finance/v2/components/DashboardInfo.tsx
+++ ui/hedgepie.finance/v2/components/DashboardInfo.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Box, Text } from 'theme-ui'
+import DashboardFunds from './DashboardFunds'
 import DashboardOverview from './DashboardOverview'
 
 function DashboardInfo() {
   return (
-    <Box sx={{ margin: '2rem 3rem', width: '100%' }}>
+    <Box sx={{ margin: '2rem 3rem', width: '100%', display: 'flex', flexDirection: 'column', gap: '20px' }}>
       <DashboardOverview />
+      <DashboardFunds />
     </Box>
   )
 }
