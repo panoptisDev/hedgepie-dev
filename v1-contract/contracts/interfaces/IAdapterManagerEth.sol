95e4e138e615601b3ae9e09730503ac5575dc274
daiki saito <daiki.saito92@gmail.com>
2022-10-20 03:27:27 +0900

    feat: remove unnecessary functions from AdapterManager contract and interface

--- v1-contract/contracts/interfaces/IAdapterManagerEth.sol
+++ v1-contract/contracts/interfaces/IAdapterManagerEth.sol
@@ -6,49 +6,4 @@ interface IAdapterManagerEth {
         external
         view
         returns (address adapterStrat);
-
-    function getDepositCallData(address _adapter, uint256 _amount)
-        external
-        view
-        returns (
-            address to,
-            uint256 value,
-            bytes memory data
-        );
-
-    function getWithdrawCallData(address _adapter, uint256 _amount)
-        external
-        view
-        returns (
-            address to,
-            uint256 value,
-            bytes memory data
-        );
-
-    function getLoanCallData(address _adapter, uint256 _amount)
-        external
-        view
-        returns (
-            address to,
-            uint256 value,
-            bytes memory data
-        );
-
-    function getDeLoanCallData(address _adapter, uint256 _amount)
-        external
-        view
-        returns (
-            address to,
-            uint256 value,
-            bytes memory data
-        );
-
-    function getEnterMarketCallData(address _adapter)
-        external
-        view
-        returns (
-            address to,
-            uint256 value,
-            bytes memory data
-        );
 }
