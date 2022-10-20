c9f66c4a7193ac780ddaa6d93537f6b775640966
S Deepak Srinivas <sdscode00@gmail.com>
2022-10-20 16:58:22 +0530

    feat : Hotjar + GA integration

--- ui/hedgepie.com/pages/_app.tsx
+++ ui/hedgepie.com/pages/_app.tsx
@@ -1,8 +1,44 @@
-import "styles/global.css";
+/* eslint-disable no-use-before-define */
+import React from "react";
 import type { AppProps } from "next/app";
 
+import Script from "next/script";
+import Router from "next/router";
+import "../styles/global.css";
+import Head from "next/head";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    hotjar.initialize(3196236, 6);
+  }, []);
+
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-G6LN726K38`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-G6LN726K38', {
+        page_path: window.location.pathname,
+        });
+        `}
+      </Script>
+
+      <Head>
+        <title>Hedge Pie</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- ui/hedgepie.com/pages/_document.tsx
+++ ui/hedgepie.com/pages/_document.tsx
@@ -1,4 +1,7 @@
+// pages/_document.js
+import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -10,6 +13,24 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         />
+        <link rel="apple-touch-icon" href="/images/logo.png"></link>
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-G6LN726K38`}
+        />
+        <Script strategy="lazyOnload">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-G6LN726K38', {
+        page_path: window.location.pathname,
+        });
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
