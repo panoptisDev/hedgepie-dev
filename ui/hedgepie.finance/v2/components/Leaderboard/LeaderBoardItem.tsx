08f84fa2e69ee0279dfb03b3250a7e4fce4ca45b
S Deepak Srinivas <sdscode00@gmail.com>
2022-11-21 20:11:38 +0530

    feat : basic setup for leaderboard grid items

---
+++ ui/hedgepie.finance/v2/components/Leaderboard/LeaderBoardItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TokenInfo } from 'v2/components/Leaderboard/LeaderboardContent'
+
+interface LeaderboardItemProps {
+  item: TokenInfo
+}
+
+function LeaderBoardItem(props: LeaderboardItemProps) {
+  return <div>LeaderBoardItem</div>
+}
+
+export default LeaderBoardItem
